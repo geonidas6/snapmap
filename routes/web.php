0f516d9009810fbd802751bc60f154dc69d2a66d
Patrice Akotse <geonidas6@gmail.com>
2025-07-08 17:06:57 +0200

    tentative de resolution

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\App;
 use App\Http\Controllers\TracemapController;
 use App\Http\Controllers\AdminController;
 
+
+
 // Route principale qui redirige vers la page d'accueil des tracemaps
 Route::get('/', function () {
     return redirect()->route('tracemap.index');
